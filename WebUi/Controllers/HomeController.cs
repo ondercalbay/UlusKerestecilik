3ffb0185d7670c3e6411b3e013ccdef895e97780
ondercalbay <ondercalbay.com>
2018-04-20 16:48:59 +0300

    bitti

---
+++ WebUi/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUi.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        // GET: Home
+        [Route("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("/Hakkimizda")]
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        [Route("/Hizmetlerimiz")]
+        public ActionResult Hizmetlerimiz()
+        {
+            return View();
+        }
+        [Route("/Iletisim")]
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
